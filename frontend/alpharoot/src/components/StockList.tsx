cd375607ec3df2b07466a358d08670ac7737b25d
KimJiGwang <5244416@naver.com>
2025-04-09 01:33:42 +0900

    feat: 첫 커밋(폴더 구조 생성)

---
+++ frontend/alpharoot/src/components/StockList.tsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react';
+import {axios} from 'axios';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const StocklistContainer = styled.div`
